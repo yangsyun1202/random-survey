01e341923c95d4ebf754a104d40e63751e7f86b2
yangsyun1202 <YANGSYUN1202@gmail.com>
2026-03-01 09:52:25 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,6 +8,8 @@
 
 
 const urls = [
-	'https://forms.gle/ThyLEePxbNp3qEkU7',
 	'https://forms.gle/7apJ8R3ajYh8xy317',
+	'https://forms.gle/ThyLEePxbNp3qEkU7',
+	'https://forms.gle/ThyLEePxbNp3qEkU7',
+	'https://forms.gle/nMMTiPAn615YbFBV6',
 ];
