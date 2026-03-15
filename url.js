acd5b9e627b7a50c4226427358a28ee9ec384fc3
yangsyun1202 <YANGSYUN1202@gmail.com>
2026-03-15 22:43:46 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,10 +8,12 @@
 
 
 const urls = [
-	'https://forms.gle/GUeUnXBLimaRfoZu5',
-	'https://forms.gle/GUeUnXBLimaRfoZu5',
-	'https://forms.gle/GUeUnXBLimaRfoZu5',
-	'https://forms.gle/o6VFZgzXnBAaM29VA',
-	'https://forms.gle/a1UXoEmVVUGqfMrx5',
-	'https://forms.gle/a1UXoEmVVUGqfMrx5',
+	'https://forms.gle/KMckmFdTBZWrUNjk6',
+	'https://forms.gle/88SaSPLwTz5UYAhK7',
+	'https://forms.gle/hjEfE8iD4mVDo5u3A',
+	'https://forms.gle/8H1aBRsLWkBwsMVh6',
+	'https://forms.gle/P8RZz5x9kmfnv5wr5',
+	'https://forms.gle/eefj2A8aLd8vSSws5',
+	'https://forms.gle/47quMoKGdzij15yY8',
+	'https://forms.gle/A1Q6SNMCmeLFx7zs7',
 ];
